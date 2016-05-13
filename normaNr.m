422e18d8614e099b442adc90b7ffb1b4fef5d55c
Stefano Cosentino <stefano286@gmail.com>
2016-05-13 19:26:25 +0100

    Delete normaNr.m

--- normaNr.m
+++
@@ -1,9 +0,0 @@
-function [Y]=normaNr(x,m,M)
-if size(x,2)==1
-    Y=(x-min(x))/(max(x)-min(x))*(M-m)+m;
-else
-    X_m=min(min(x));
-    X_M=max(max(x));
-    Y=(x-X_m)/(X_M-X_m)*(M-m)+m;
-end
-end %EoF
